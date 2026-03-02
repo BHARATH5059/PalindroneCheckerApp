27990dad94435709cdbd69033b4dfc389531b32a
BHARATH5059 <mopadabharath5059@gmail.com>
2026-03-02 15:16:40 +0530

    UC4-Character-Array-Based-Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,8 +1,40 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    public static void main(String args[]) {
-        System.out.println("PalindromeChecker");
-        System.out.print("2 nd branch ");
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Palindrome Checker App (UC4) ===");
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Convert String to Character Array
+        char[] characters = input.toCharArray();
+
+        // Two-pointer technique
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }
