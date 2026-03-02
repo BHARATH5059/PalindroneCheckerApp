6c1c39e871728132fdf175f7419770863dcb6562
BHARATH5059 <mopadabharath5059@gmail.com>
2026-03-02 15:41:31 +0530

    UC7-Deque-Based-Optimized-Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,8 +1,38 @@
 import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class PalindromeCheckerApp {
-    public static void main(String args[]) {
-        System.out.println("PalindromeChecker");
-        System.out.print("2 nd branch ");
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Palindrome Checker App (UC7 - Deque Based) ===");
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }
